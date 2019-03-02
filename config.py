810013df43a8536f53fc9171088badf6919bb032
Ryan Holmes <holmes.ryan.90@gmail.com>
2019-03-02 11:34:41 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+import datetime
+import os
+
+# -----------------------------------------------------
+# Application configurations
+# ------------------------------------------------------
+DEBUG = True
+SECRET_KEY = os.environ['SECRET_KEY'].encode()
+PORT = 5015
+HOST = 'localhost'
+
+# -----------------------------------------------------
+# SQL Alchemy configs
+# -----------------------------------------------------
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+# -----------------------------------------------------
+# ESI Configs
+# -----------------------------------------------------
+ESI_DATASOURCE = 'tranquility'  # Change it to 'singularity' to use the test server
+ESI_SWAGGER_JSON = 'https://esi.tech.ccp.is/latest/swagger.json?datasource=%s' % ESI_DATASOURCE
+ESI_SECRET_KEY = os.environ['ESI_SECRET_KEY']  # your secret key
+ESI_CLIENT_ID = '0b3d8699bab149a18838d5183507b5ab'  # your client ID
+ESI_CALLBACK = 'https://www.pyfa.io/sso/callback' # the callback URI you gave CCP
+ESI_USER_AGENT = 'pyfa-esipy'
+
+
+# ------------------------------------------------------
+# Session settings for flask login
+# ------------------------------------------------------
+PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=30)
+
+# ------------------------------------------------------
+# DO NOT EDIT
+# Fix warnings from flask-sqlalchemy / others
+# ------------------------------------------------------
+SQLALCHEMY_TRACK_MODIFICATIONS = True
